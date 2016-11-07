54f3db14e038494afbc96b4e40bcadc275a21b93
Ryosuke Kishiyanagi <g.shorewillow@gmail.com>
2016-11-08 00:34:28 +0900

    ダミーのJIRAに登録ボタンを設置

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -8,3 +8,5 @@ console.log($('.key>strong')[0].innerText);
 console.log($('#issuecard>.summary')[0].innerText);
 // 詳細
 console.log($('#issueDescription>div').html());
+
+$('.editMenu').prepend('<input type="button" class="Btn-orange" value="この課題を JIRA に登録" onclick="copyIssue(this.form);" style="float:left; padding:5px 0; width:160px;">');
