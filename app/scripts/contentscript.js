35bd4396db772aa216ba873a3357ee2c85807e5c
Ryosuke Kishiyanagi <g.shorewillow@gmail.com>
2016-11-07 22:46:35 +0900

    jquery インストール。バックログ課題DOM下調べ）

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo! Content script');
+var $ = require('jquery')
+
+//種別
+console.log($('#issuecard>.key>.issue-type-name')[0].innerText);
+// キー
+console.log($('.key>strong')[0].innerText);
+// タイトル
+console.log($('#issuecard>.summary')[0].innerText);
+// 詳細
+console.log($('#issueDescription>div').html());
