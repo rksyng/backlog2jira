3ba71aeb5393f1fb999f764a6272e7921aa3ed4a
Ryosuke Kishiyanagi <g.shorewillow@gmail.com>
2016-11-09 23:54:07 +0900

    雑多な変更

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,4 +1,4 @@
-var $ = require('jquery')
+import $ from 'jquery'
 
 //種別
 console.log($('#issuecard>.key>.issue-type-name')[0].innerText);
@@ -7,6 +7,6 @@ console.log($('.key>strong')[0].innerText);
 // タイトル
 console.log($('#issuecard>.summary')[0].innerText);
 // 詳細
-console.log($('#issueDescription>div').html());
+console.log($('#issueDescription').html());
 
 $('.editMenu').prepend('<input type="button" class="Btn-orange" value="この課題を JIRA に登録" onclick="copyIssue(this.form);" style="float:left; padding:5px 0; width:160px;">');
