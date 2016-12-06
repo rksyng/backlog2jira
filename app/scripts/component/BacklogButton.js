98c7e6a76300d945f0d1d95855568467e2e1696a
岸柳 良介 <kishiyangi@faith.co.jp>
2016-12-06 11:04:02 +0900

    bind だったのか。。。

--- app/scripts/component/BacklogButton.js
+++ app/scripts/component/BacklogButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class BacklogButton extends React.Component {
 
@@ -9,29 +8,18 @@ export default class BacklogButton extends React.Component {
   }
 
   sendToJIRA() {
-    chrome.runtime.sendMessage({
-      type:  ReactDOM.findDOMNode(this.refs.type).value,
-      key:   ReactDOM.findDOMNode(this.refs.key).value,
-      title: ReactDOM.findDOMNode(this.refs.title).value,
-      url:   ReactDOM.findDOMNode(this.refs.url).value
-    }, (response) => {
+    chrome.runtime.sendMessage(this.props.backlogIssue, (response) => {
         console.log(response);
     });
   }
 
   render() {
     return (
-      <span>
-        <input type = "button"
-               onClick = {this.sendToJIRA}
-               className = "Btn-orange"
-               style = {{float: "left", padding: "3px 0", width: "160px"}}
-               value = {chrome.i18n.getMessage('createJIRAIssue')} />;
-        <input type="hidden" ref="type" value={this.props.backlogIssue.type} />
-        <input type="hidden" ref="key" value={this.props.backlogIssue.key} />
-        <input type="hidden" ref="title" value={this.props.backlogIssue.title} />
-        <input type="hidden" ref="url" value={this.props.backlogIssue.url} />
-      </span>
+      <input type = "button"
+             onClick = {this.sendToJIRA}
+             className = "Btn-orange"
+             style = {{float: "left", padding: "3px 0", width: "160px"}}
+             value = {chrome.i18n.getMessage('createJIRAIssue')} />
     )
   }
 }
